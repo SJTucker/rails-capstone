d261fc96627e61cd797608b71c561454c76720ea
Sam Tucker <samueljeffersontucker@gmail.com>
2014-06-17 12:24:17 -0500

    rails generate active_admin install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name, :phone_number, :credit_card
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :phone_number, :credit_card) }
   end
 end
