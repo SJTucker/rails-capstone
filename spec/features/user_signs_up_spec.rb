e6f6c18a7dc2b1c5bdcddeb3021e4b6a0e6e6b11
Sam Tucker <samueljeffersontucker@gmail.com>
2014-06-17 11:32:37 -0500

    added permitted paramaters to devise controller

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -9,10 +9,27 @@ feature "User signs up" do
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     fill_in "Credit card", with: "1234"
-    
+
     click_button "Sign up"
     page.should have_content "We hope you enjoy auTASTIC!"
     page.should_not have_link("I'm Ready to TASTE!")
-
   end
+
+  scenario "failed signup" do
+    User.create(email: "joe@example.com", name: "joe", password: "password", password_confirmation: "password")
+    visit '/'
+    click_link "I'm Ready to TASTE!"
+    fill_in "Email", with: "joe@example.com"
+    fill_in "Name", with: "joe"
+    fill_in "Password", with: "mypassword"
+    fill_in "Password confirmation", with: "notthesame"
+
+    click_button "Sign up"
+    #page.should_not have_content "Welcome to "
+    page.should have_content "Your account could not be created."
+
+    page.should have_error("already exists", on: "Email")
+    page.should have_error("doesn't match password", on:"Password")
+  end
+
 end
